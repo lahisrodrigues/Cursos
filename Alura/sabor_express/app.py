465b7a953373f9d70eea70849dbb692f4cb5b012
Lais Rodrigues <laispr96@gmail.com>
2026-06-01 23:08:35 -0300

    Studying

--- Alura/sabor_express/app.py
+++ Alura/sabor_express/app.py
@@ -1,8 +1,41 @@
-print('Sabor Express\n')
-print ('1. Cadastrar restaurante')
-print ('2. Listar restaurante')
-print ('3. Ativar restaurante')
-print ('4. Sair\n')
+import os
 
-opcao_escolhida = input('Escolha uma opção: ')
-print ('Vocês escolheu a opção', opcao_escolhida)
+def exibir_nome_do_programa():
+    print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+""")
+
+
+def exibir_opcoes():
+    print ('1. Cadastrar restaurante')
+    print ('2. Listar restaurantes')
+    print ('3. Ativar restaurante')
+    print ('4. Sair\n')
+
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando o app...')
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        finalizar_app()
+
+def main(): # define os passos pro programa funcionar
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
