20f6a2d6950deb06d684a721bb35f20fc4e58704
Lais Rodrigues <laispr96@gmail.com>
2026-06-02 23:03:01 -0300

    Studying

--- Alura/Python/sabor_express/app.py
+++ Alura/Python/sabor_express/app.py
@@ -21,21 +21,34 @@ def finalizar_app():
     os.system('cls')
     print('Finalizando o app...\n')
 
-def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurante')
-    else:
-        finalizar_app()
+def opcao_invalida():
+    print('Opção inválida!\n')
+    input('Digite uma tecla para voltar para o menu principal ')
+    main()
 
-def main(): # define os passos pro programa funcionar
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        if opcao_escolhida == 1:
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
 
 if __name__ == '__main__':
     main()
+
+    #####parei em Listas
