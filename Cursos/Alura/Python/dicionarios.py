778e1c2554538373074ef4abe73fa4a1d46de021
lahisrodrigues <laispr96@gmail.com>
2026-06-22 14:19:08 -0300

    Update dicionarios.py

--- Cursos/Alura/Python/dicionarios.py
+++ Cursos/Alura/Python/dicionarios.py
@@ -5,3 +5,10 @@ pessoa['profissão'] = 'engenheiro'
 del pessoa['cidade']
 print(pessoa)
 
+
+meus_quadrados = {}
+for x in range(1,6):
+    conta = x * x
+    meus_quadrados[x] = conta
+
+print(meus_quadrados)
