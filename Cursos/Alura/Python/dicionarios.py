385ed1d008d524730a425afa924909324b3c59ca
lahisrodrigues <laispr96@gmail.com>
2026-06-22 14:43:10 -0300

    Update dicionarios.py

--- Cursos/Alura/Python/dicionarios.py
+++ Cursos/Alura/Python/dicionarios.py
@@ -1,14 +1,20 @@
+# exercício 1 e 2
 pessoa = {'nome': 'lais', 'idade': '29', 'cidade': 'Pouso Alegre'}
-
 pessoa['idade'] = 30
 pessoa['profissão'] = 'engenheiro'
 del pessoa['cidade']
 print(pessoa)
 
-
+# exercício 3
 meus_quadrados = {}
 for x in range(1,6):
     conta = x * x
     meus_quadrados[x] = conta
-
 print(meus_quadrados)
+
+# exercício 4
+pessoa = {'nome': 'lais', 'idade': '29', 'cidade': 'Pouso Alegre'}
+if 'nome' in pessoa:
+    print('Existe essa chave')
+else:
+    print('Não existe essa chave')
