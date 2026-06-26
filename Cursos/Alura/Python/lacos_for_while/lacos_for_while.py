a6b13140bbf110b9f417beedfdc73650de3f47d0
lahisrodrigues <laispr96@gmail.com>
2026-06-26 17:26:16 -0300

    Update lacos_for_while.py

--- Cursos/Alura/Python/lacos_for_while/lacos_for_while.py
+++ Cursos/Alura/Python/lacos_for_while/lacos_for_while.py
@@ -1,3 +1,18 @@
 # Atividade 1
 
-variavel = 0
+clientes = ["João", "Maria", "Carlos", "Ana", "Beatriz"]
+
+for nome in clientes:
+    print(nome)
+
+
+# Atividade 2
+
+contador = 0
+
+while contador < 10:
+     print("Processando dados")
+     contador += 1
+
+# Atividade 3
+
